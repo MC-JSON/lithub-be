1debb4792368ca660df8aac85526a56a5c5b6ea6
Jason McCoy <jwmccoy29@gmail.com>
2022-06-07 11:28:34 -0500

    auth

---
+++ utilities/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+const TOKEN_KEY = process.env.TOKEN_KEY || 'areallylonggoodkey'
+
+const restrict = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    if (jwt.verify(token, TOKEN_KEY)) {
+      next()
+    }
+  } catch (error) {
+    res.status(403).send('Unauthorized')
+  }
+}
+
+export default restrict
